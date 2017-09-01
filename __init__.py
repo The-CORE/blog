2737de380811a4f5d13c0a4479661ba90f9f63d8
James Wright <booff42@bigpond.com>
2017-09-01 21:32:57 +1000

    Added time stuff, and some other things.

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 import os
 import json
 import markdown
+from datetime import datetime
 
 
 ROOT = os.path.dirname(__file__)
@@ -67,6 +68,42 @@ def build_index():
     this constructs an index page.
     """
     pass
+
+
+def get_year_decimal_time():
+    """
+    Gets the current time as a decimal of the year.
+    """
+    now = datetime.now()
+    year, month, day, hour, minute, second, microsecond = now.year, now.month, \
+        now.day, now.hour, now.minute, now.second, now.microsecond
+    is_leap_year = year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+    days_in_months = [
+        None,
+        31,
+        29 if is_leap_year else 28,
+        31,
+        30,
+        31,
+        30,
+        31,
+        31,
+        30,
+        31,
+        30,
+        31
+    ]
+
+    # 24 * 60 * 60 * 1000000 = 86400000000
+    # Also below is just for show really.
+    microseconds_in_this_year = (366 if is_leap_year else 365) * 86400000000
+    days_into_this_year = day + sum(days_in_months[1:month])
+    hours_into_this_year = days_into_this_year * 24 + hour
+    minutes_into_this_year = hours_into_this_year * 60 + minute
+    seconds_into_this_year = minutes_into_this_year * 60 + second
+    microseconds_into_this_year = seconds_into_this_year * 1000000 + microsecond
+
+    return year + microseconds_into_this_year / microseconds_in_this_year
 
 
 if __name__ == '__main__':
